a849093260ddbd274237036706dab55416b7e66c
EdivanAzevedo <66445757+EdivanAzevedo@users.noreply.github.com>
2023-07-17 17:01:18 -0300

    Correção do SQL
    Correção de erro do slq do data_fim

--- Banco_de_dados.sql
+++ Banco_de_dados.sql
@@ -1,38 +1,208 @@
-CREATE TABLE pessoa (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT
-);
-CREATE TABLE projeto (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    titulo VARCHAR(250) NOT NULL,
-    descricao VARCHAR(250) NOT NULL,
-    data_inicial TIMESTAMP NOT NULL,
-    data_fim TIMESTAMP NOT NULL
-);
-CREATE TABLE pessoa_projeto(
-    id_pessoa INT NOT NULL,
-    id_projeto INT NOT NULL,
-    FOREIGN KEY (id_pessoa) REFERENCES pessoa(id),
-    FOREIGN KEY (id_projeto) REFERENCES projeto(id)
-);
-CREATE TABLE diario (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT
-);
-CREATE TABLE horario_atendimento (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    data_ini TIMESTAMP NOT NULL,
-    data_fim TIMESTAMP NOT NULL,
-    horario_ini TIME NOT NULL,
-    horario_fim TIME NOT NULL
-);
-CREATE TABLE diario_horario(
-    id_diario INT NOT NULL,
-    id_horario INT NOT NULL,
-    FOREIGN KEY (id_diario) REFERENCES diario(id),
-    FOREIGN KEY (id_horario) REFERENCES horario_atendimento(id)
-);
-CREATE TABLE projeto_horario(
-    id_projeto INT NOT NULL,
-    id_horario INT NOT NULL,
-    FOREIGN KEY (id_projeto) REFERENCES projeto(id),
-    FOREIGN KEY (id_horario) REFERENCES horario_atendimento(id)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 17/07/2023 às 22:00
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `sistema_academico`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `diario`
+--
+
+CREATE TABLE `diario` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `diario_horario`
+--
+
+CREATE TABLE `diario_horario` (
+  `id_diario` int(11) NOT NULL,
+  `id_horario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `horario_atendimento`
+--
+
+CREATE TABLE `horario_atendimento` (
+  `id` int(11) NOT NULL,
+  `data_hora_ini` datetime NOT NULL,
+  `data_hora_fim` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pessoa`
+--
+
+CREATE TABLE `pessoa` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pessoa_projeto`
+--
+
+CREATE TABLE `pessoa_projeto` (
+  `id_pessoa` int(11) NOT NULL,
+  `id_projeto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `projeto`
+--
+
+CREATE TABLE `projeto` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(250) NOT NULL,
+  `descricao` varchar(250) NOT NULL,
+  `data_inicial` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `data_fim` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `projeto_horario`
+--
+
+CREATE TABLE `projeto_horario` (
+  `id_projeto` int(11) NOT NULL,
+  `id_horario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `diario`
+--
+ALTER TABLE `diario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `diario_horario`
+--
+ALTER TABLE `diario_horario`
+  ADD KEY `id_diario` (`id_diario`),
+  ADD KEY `id_horario` (`id_horario`);
+
+--
+-- Índices de tabela `horario_atendimento`
+--
+ALTER TABLE `horario_atendimento`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `pessoa`
+--
+ALTER TABLE `pessoa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `pessoa_projeto`
+--
+ALTER TABLE `pessoa_projeto`
+  ADD KEY `id_pessoa` (`id_pessoa`),
+  ADD KEY `id_projeto` (`id_projeto`);
+
+--
+-- Índices de tabela `projeto`
+--
+ALTER TABLE `projeto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `projeto_horario`
+--
+ALTER TABLE `projeto_horario`
+  ADD KEY `id_projeto` (`id_projeto`),
+  ADD KEY `id_horario` (`id_horario`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `diario`
+--
+ALTER TABLE `diario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `horario_atendimento`
+--
+ALTER TABLE `horario_atendimento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `pessoa`
+--
+ALTER TABLE `pessoa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `projeto`
+--
+ALTER TABLE `projeto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `diario_horario`
+--
+ALTER TABLE `diario_horario`
+  ADD CONSTRAINT `diario_horario_ibfk_1` FOREIGN KEY (`id_diario`) REFERENCES `diario` (`id`),
+  ADD CONSTRAINT `diario_horario_ibfk_2` FOREIGN KEY (`id_horario`) REFERENCES `horario_atendimento` (`id`);
+
+--
+-- Restrições para tabelas `pessoa_projeto`
+--
+ALTER TABLE `pessoa_projeto`
+  ADD CONSTRAINT `pessoa_projeto_ibfk_1` FOREIGN KEY (`id_pessoa`) REFERENCES `pessoa` (`id`),
+  ADD CONSTRAINT `pessoa_projeto_ibfk_2` FOREIGN KEY (`id_projeto`) REFERENCES `projeto` (`id`);
+
+--
+-- Restrições para tabelas `projeto_horario`
+--
+ALTER TABLE `projeto_horario`
+  ADD CONSTRAINT `projeto_horario_ibfk_1` FOREIGN KEY (`id_projeto`) REFERENCES `projeto` (`id`),
+  ADD CONSTRAINT `projeto_horario_ibfk_2` FOREIGN KEY (`id_horario`) REFERENCES `horario_atendimento` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
